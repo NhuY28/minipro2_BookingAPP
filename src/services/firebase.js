60ef6ff610d119b9ea70f25ad55c748f00bbc447
MaiNhuY_23IT326 <166639420+NhuY28@users.noreply.github.com>
2026-09-26 01:14:12 +0700

    2nd

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,46 @@
+// import { initializeApp } from "firebase/app";
+// import { getDatabase } from "firebase/database";
+
+// import {
+//   initializeAuth,
+//   getReactNativePersistence,
+// } from "firebase/auth";
+
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// const firebaseConfig = {
+//   apiKey:
+//     process.env.EXPO_PUBLIC_FIREBASE_API_KEY ??
+//     "AIzaSyCDTwQfMF_AZw4CpUSSoclRd8jpaDUke90",
+//   authDomain: "study-room-booking-a9fc9.firebaseapp.com",
+//   projectId: "study-room-booking-a9fc9",
+//   storageBucket: "study-room-booking-a9fc9.firebasestorage.app",
+//   messagingSenderId: "417051803462",
+//   appId: "1:417051803462:web:cdd41424e62548afe26f47",
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// // Firebase Realtime Database
+// export const db = getDatabase(app);
+
+// // Firebase Authentication
+// export const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(AsyncStorage),
+// });
+
+
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 import {
+  getAuth,
   initializeAuth,
   getReactNativePersistence,
 } from "firebase/auth";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
 const firebaseConfig = {
   apiKey:
@@ -25,6 +59,11 @@ const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 
 // Firebase Authentication
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+export const auth =
+  Platform.OS === "web"
+    ? getAuth(app)
+    : initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+      });
+
+export { app };
